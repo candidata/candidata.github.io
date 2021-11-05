306feee821b98d002c1383ea0d6d9ccea0ed1c6a
Felipe <felipevasconceloslpz@gmail.com>
2021-11-04 21:23:47 -0300

    quase la

--- pages/estatisticas.js
+++ pages/estatisticas.js
@@ -1,4 +1,31 @@
 
+
+var dadosOcupacoes2020 = [['OUTROS', 66057],
+['AGRICULTOR', 27040],
+['SERVIDOR PUBLICO MUNICIPAL', 18464],
+['EMPRESARIO', 18151]];
+var dadosOcupacoes2018 = [['OUTROS', 5690],
+['EMPRESARIO', 3024],
+['ADVOGADO', 1832],
+['DEPUTADO', 1126]];
+var dadosOcupacoes2016 = [['OUTROS', 48691],
+['AGRICULTOR', 25190],
+['COMERCIANTE', 15739],
+['SERVIDOR PUBLICO MUNICIPAL', 15630]];
+var dadosOcupacoes2002 = [['NAO INFORMADA', 2533],
+['ADVOGADO', 1559],
+['COMERCIANTE', 1356],
+['OUTROS', 1198]];
+var dadosOcupacoes2006 = [['OUTROS', 2735],
+['ADVOGADO', 1549],
+['COMERCIANTE', 1496],
+['EMPRESARIO', 1482]];
+var dadosOcupacoes2010 = [['OUTROS', 3578],
+['EMPRESARIO', 1928],
+['ADVOGADO', 1355],
+['COMERCIANTE', 1135]];
+
+
 var ctx = document.getElementById("chart-bars").getContext("2d");
 
 new Chart(ctx, {
@@ -85,9 +112,9 @@ var ctx2 = document.getElementById("chart-line").getContext("2d");
 new Chart(ctx2, {
     type: "line",
     data: {
-    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: [1945, 1966, 1986,2006, 2020],
     datasets: [{
-        label: "Mobile apps",
+        label: "Porcentagem",
         tension: 0,
         borderWidth: 0,
         pointRadius: 5,
@@ -98,7 +125,7 @@ new Chart(ctx2, {
         borderWidth: 4,
         backgroundColor: "transparent",
         fill: true,
-        data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
+        data: [2.69,2.76,8.5,14.35,33.7],
         maxBarThickness: 6
 
     }],
@@ -166,6 +193,252 @@ new Chart(ctx2, {
 var ctx3 = document.getElementById("chart-line-tasks").getContext("2d");
 
 new Chart(ctx3, {
+    type: "line",
+    data: {
+    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+        label: "Mobile apps",
+        tension: 0,
+        borderWidth: 0,
+        pointRadius: 5,
+        pointBackgroundColor: "rgba(255, 255, 255, .8)",
+        pointBorderColor: "transparent",
+        borderColor: "rgba(255, 255, 255, .8)",
+        borderWidth: 4,
+        backgroundColor: "transparent",
+        fill: true,
+        data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+        maxBarThickness: 6
+
+    }],
+    },
+    options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+        display: false,
+        }
+    },
+    interaction: {
+        intersect: false,
+        mode: 'index',
+    },
+    scales: {
+        y: {
+        grid: {
+            drawBorder: false,
+            display: true,
+            drawOnChartArea: true,
+            drawTicks: false,
+            borderDash: [5, 5],
+            color: 'rgba(255, 255, 255, .2)'
+        },
+        ticks: {
+            display: true,
+            padding: 10,
+            color: '#f8f9fa',
+            font: {
+            size: 14,
+            weight: 300,
+            family: "Roboto",
+            style: 'normal',
+            lineHeight: 2
+            },
+        }
+        },
+        x: {
+        grid: {
+            drawBorder: false,
+            display: false,
+            drawOnChartArea: false,
+            drawTicks: false,
+            borderDash: [5, 5]
+        },
+        ticks: {
+            display: true,
+            color: '#f8f9fa',
+            padding: 10,
+            font: {
+            size: 14,
+            weight: 300,
+            family: "Roboto",
+            style: 'normal',
+            lineHeight: 2
+            },
+        }
+        },
+    },
+    },
+});
+
+
+var ctx4 = document.getElementById("grafico4").getContext("2d");
+new Chart(ctx4, {
+    type: "line",
+    data: {
+    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+        label: "Mobile apps",
+        tension: 0,
+        borderWidth: 0,
+        pointRadius: 5,
+        pointBackgroundColor: "rgba(255, 255, 255, .8)",
+        pointBorderColor: "transparent",
+        borderColor: "rgba(255, 255, 255, .8)",
+        borderWidth: 4,
+        backgroundColor: "transparent",
+        fill: true,
+        data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+        maxBarThickness: 6
+
+    }],
+    },
+    options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+        display: false,
+        }
+    },
+    interaction: {
+        intersect: false,
+        mode: 'index',
+    },
+    scales: {
+        y: {
+        grid: {
+            drawBorder: false,
+            display: true,
+            drawOnChartArea: true,
+            drawTicks: false,
+            borderDash: [5, 5],
+            color: 'rgba(255, 255, 255, .2)'
+        },
+        ticks: {
+            display: true,
+            padding: 10,
+            color: '#f8f9fa',
+            font: {
+            size: 14,
+            weight: 300,
+            family: "Roboto",
+            style: 'normal',
+            lineHeight: 2
+            },
+        }
+        },
+        x: {
+        grid: {
+            drawBorder: false,
+            display: false,
+            drawOnChartArea: false,
+            drawTicks: false,
+            borderDash: [5, 5]
+        },
+        ticks: {
+            display: true,
+            color: '#f8f9fa',
+            padding: 10,
+            font: {
+            size: 14,
+            weight: 300,
+            family: "Roboto",
+            style: 'normal',
+            lineHeight: 2
+            },
+        }
+        },
+    },
+    },
+});
+
+
+var ctx5 = document.getElementById("grafico5").getContext("2d");
+new Chart(ctx5, {
+    type: "line",
+    data: {
+    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+        label: "Mobile apps",
+        tension: 0,
+        borderWidth: 0,
+        pointRadius: 5,
+        pointBackgroundColor: "rgba(255, 255, 255, .8)",
+        pointBorderColor: "transparent",
+        borderColor: "rgba(255, 255, 255, .8)",
+        borderWidth: 4,
+        backgroundColor: "transparent",
+        fill: true,
+        data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+        maxBarThickness: 6
+
+    }],
+    },
+    options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+        display: false,
+        }
+    },
+    interaction: {
+        intersect: false,
+        mode: 'index',
+    },
+    scales: {
+        y: {
+        grid: {
+            drawBorder: false,
+            display: true,
+            drawOnChartArea: true,
+            drawTicks: false,
+            borderDash: [5, 5],
+            color: 'rgba(255, 255, 255, .2)'
+        },
+        ticks: {
+            display: true,
+            padding: 10,
+            color: '#f8f9fa',
+            font: {
+            size: 14,
+            weight: 300,
+            family: "Roboto",
+            style: 'normal',
+            lineHeight: 2
+            },
+        }
+        },
+        x: {
+        grid: {
+            drawBorder: false,
+            display: false,
+            drawOnChartArea: false,
+            drawTicks: false,
+            borderDash: [5, 5]
+        },
+        ticks: {
+            display: true,
+            color: '#f8f9fa',
+            padding: 10,
+            font: {
+            size: 14,
+            weight: 300,
+            family: "Roboto",
+            style: 'normal',
+            lineHeight: 2
+            },
+        }
+        },
+    },
+    },
+});
+
+
+var ctx6 = document.getElementById("grafico6").getContext("2d");
+new Chart(ctx6, {
     type: "line",
     data: {
     labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
